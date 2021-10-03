c0cd80085466943a4de4ccbcd4564193577dd028
ElhamFadel <elham2000Fadel@gmail.com>
2021-10-03 19:43:11 +0300

    add route cart

--- server/database/queries/cart.js
+++ server/database/queries/cart.js
@@ -1,11 +1,9 @@
-import connection from '../connection';
+const connection = require('../connection');
 
 module.exports = {
-  getCart: (userName) => connection
-    .query('SELECT * FROM cart WHERE username = ?', [userName]),
-
-  addToCart: (userName, productId, quantity) => connection.query('INSERT INTO cart (username, id, quantity) VALUES (?, ?, ?)', [userName, productId, quantity]),
-  deleteFromCart: (userName, productId) => connection.query('DELETE FROM cart WHERE username = ? AND id = ?', [userName, productId]),
-  updateCart: (userName, productId, quantity) => connection.query('UPDATE cart SET quantity = ? WHERE username = ? AND id = ?', [quantity, userName, productId]),
-  getCartTotal: (userName) => connection.query('SELECT SUM(cart.quantity * products.price) AS total FROM cart INNER JOIN products ON cart.id = products.id WHERE username = ?', [userName]),
+  getCart: (userName) => connection.query('SELECT * FROM cart WHERE username = $1', [userName]),
+  addToCart: (userName, productId, quantity) => connection.query('INSERT INTO cart (username, id, quantity) VALUES ($1, $2, $3)', [userName, productId, quantity]),
+  deleteFromCart: (userName, productId) => connection.query('DELETE FROM cart WHERE username = $1 AND id = $2', [userName, productId]),
+  updateCart: (userName, productId, quantity) => connection.query('UPDATE cart SET quantity = $1 WHERE username = $2 AND id = $3', [quantity, userName, productId]),
+  getCartTotal: (userName) => connection.query('SELECT SUM(cart.quantity * products.price) AS total FROM cart INNER JOIN products ON cart.id = products.id WHERE username = $1', [userName]),
 };

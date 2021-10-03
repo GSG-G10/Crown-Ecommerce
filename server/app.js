7dcfa77bd02a796ad3cc60ebf29107c9beb57f5f
divluffy <div.jo2022@gmail.com>
2021-10-03 12:13:41 +0300

    fix port and router

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+require('env2')('config.env');
+const { join } = require('path');
+const cookieParser = require('cookie-parser');
+const router = require('./routes/index');
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set('port', process.env.PORT || 8080);
+
+app.use('/api/v1/', router);
+
+if (NODE_ENV === 'production') {
+  app.use(express.static(join(__dirname, '..', 'client', 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(join(__dirname, '..', 'client', 'build', 'index.html'));
+  });
+}
+
+module.exports = app;

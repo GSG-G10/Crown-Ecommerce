36a6a29cf59676afc4caf9f05d1e42b4b7cf81f4
ElhamFadel <elham2000Fadel@gmail.com>
2021-10-03 19:04:52 +0300

    edit get cart total query

--- server/database/queries/cart.js
+++ server/database/queries/cart.js
@@ -7,5 +7,5 @@ module.exports = {
   addToCart: (userName, productId, quantity) => connection.query('INSERT INTO cart (username, id, quantity) VALUES (?, ?, ?)', [userName, productId, quantity]),
   deleteFromCart: (userName, productId) => connection.query('DELETE FROM cart WHERE username = ? AND id = ?', [userName, productId]),
   updateCart: (userName, productId, quantity) => connection.query('UPDATE cart SET quantity = ? WHERE username = ? AND id = ?', [quantity, userName, productId]),
-  getCartTotal: (userName) => connection.query('SELECT SUM(quantity * price) AS total FROM cart INNER JOIN products ON cart.id = products.id WHERE username = ?', [userName]),
+  getCartTotal: (userName) => connection.query('SELECT SUM(cart.quantity * products.price) AS total FROM cart INNER JOIN products ON cart.id = products.id WHERE username = ?', [userName]),
 };

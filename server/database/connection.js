902a63e6be5efaba30eeebd6dce6e3da8f6d9b1b
Your Name <noorin.sa.99@gmail.com>
2021-10-03 16:58:41 +0300

    setup database connection Relates #14

---
+++ server/database/connection.js
@@ -0,0 +1,24 @@
+const { Pool } = require('pg');
+require('env2')('./config.env');
+const { DB_URL, DEV_DB_URL, NODE_ENV } = process.env;
+
+let dbUrl = '';
+
+switch (NODE_ENV) {
+    case 'production':
+        dbUrl = DB_URL;
+        break;
+    case 'development':
+        dbUrl = DEV_DB_URL;
+        break;
+    default:
+        throw new Error('No database found');
+}
+
+
+const options = {
+    connectionString: dbUrl,
+    ssl: { rejectUnauthorized: false },
+};
+
+module.exports = new Pool(options);

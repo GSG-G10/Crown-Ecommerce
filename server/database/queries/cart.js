ecef270821b68156dba19c75e1f5ae1417e077e9
ElhamFadel <elham2000Fadel@gmail.com>
2021-10-03 19:03:24 +0300

    add all queries relates cart #15 #9 #8

---
+++ server/database/queries/cart.js
@@ -0,0 +1,11 @@
+import connection from '../connection';
+
+module.exports = {
+  getCart: (userName) => connection
+    .query('SELECT * FROM cart WHERE username = ?', [userName]),
+
+  addToCart: (userName, productId, quantity) => connection.query('INSERT INTO cart (username, id, quantity) VALUES (?, ?, ?)', [userName, productId, quantity]),
+  deleteFromCart: (userName, productId) => connection.query('DELETE FROM cart WHERE username = ? AND id = ?', [userName, productId]),
+  updateCart: (userName, productId, quantity) => connection.query('UPDATE cart SET quantity = ? WHERE username = ? AND id = ?', [quantity, userName, productId]),
+  getCartTotal: (userName) => connection.query('SELECT SUM(quantity * price) AS total FROM cart INNER JOIN products ON cart.id = products.id WHERE username = ?', [userName]),
+};

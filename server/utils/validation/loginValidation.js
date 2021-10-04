8d2797351f2251d9cda4fcccefc0bfe138bbbb67
AhemdAboAbadi <ahmed.free.py@gmail.com>
2021-10-04 10:45:13 +0300

    login validation

---
+++ server/utils/validation/loginValidation.js
@@ -0,0 +1,8 @@
+const joi = require('joi');
+
+const loginSchema = joi.object({
+  email: joi.string().email().required(),
+  password: joi.string().min(7).required(),
+});
+
+module.exports = loginSchema;

3d293fe0dc46f5c3d6cd793f63bb511d0d74200b
AhemdAboAbadi <ahmed.free.py@gmail.com>
2021-10-05 19:51:43 +0300

    Setup Switch & Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import "./App.css";
+
+import { Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      Hi from App
+      <Switch>
+        <Route exact path="/">
+          homepage
+        </Route>
+        <Route exact path="/fav">
+          fav page
+        </Route>
+        <Route exact path="/user">
+          user page
+        </Route>
+        <Route exact path="/card">
+          card page
+        </Route>
+      </Switch>
     </div>
   );
 }

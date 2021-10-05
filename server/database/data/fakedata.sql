d8462ed1731d7892e466bf973e5e7e8e4427ce0e
Your Name <noorin.sa.99@gmail.com>
2021-10-05 10:26:22 +0300

    change passwords in users table and changing the syntax Relates #26

--- server/database/data/fakedata.sql
+++ server/database/data/fakedata.sql
@@ -1,59 +1,68 @@
-INSERT INTO USERS (username,f_name, email, pasword) VALUES ('noorin','Noor','noor@gmail.com', '1234');
-INSERT INTO USERS (username,f_name, email, pasword) VALUES ('elham','Elham','elham@gmail.com', '12345');
-INSERT INTO USERS (username,f_name, email, pasword) VALUES ('ibrahim','Ibrahim','ibrahim@gmail.com', '123456');
+INSERT INTO USERS (username,f_name, email, pasword) 
+VALUES 
+  ('noorin','Noor','noor@gmail.com', '$2a$10$AkqtzOyH/HU8srfRDauQouKfE1vSA0PmKA8UjLDX.acEr.jc7xSMW'),
+  ('elham','Elham','elham@gmail.com', '$2a$10$d0xD86pbtps5Qsci0KEqKuMvQ75sSvY76iMciv/sgJwbUOJmCFJtC');
+ 
 
 
-INSERT INTO PRODUCTS (title, category, price, image1,image2,image3,image4) VALUES('this girl is on fire','women',25,
+INSERT INTO PRODUCTS (title, category, price, image1,image2,image3,image4) 
+VALUES
+('this girl is on fire','women',25,
 'https://image.spreadshirtmedia.com/image-server/v1/mp/products/T943A231MPA3084PT17X67Y22D1025124049FS3247/views/1,width=800,height=800,appearanceId=231,backgroundColor=F2F2F2,modelId=320,crop=detail/this-girl-is-on-fire-womens-rolled-sleeve-t-shirt.jpg',
 'https://image.spreadshirtmedia.com/image-server/v1/mp/products/T943A231MPA3084PT17X67Y22D1025124049FS3247/views/1,width=800,height=800,appearanceId=231,backgroundColor=F2F2F2,modelId=2294,crop=detail/this-girl-is-on-fire-womens-rolled-sleeve-t-shirt.jpg',
 'https://image.spreadshirtmedia.com/image-server/v1/mp/productTypes/943/views/2/appearances/231,width=800,height=800,backgroundColor=F2F2F2,modelId=326,crop=detail.jpg',
-'https://image.spreadshirtmedia.com/image-server/v1/mp/productTypes/943/views/2/appearances/231,width=800,height=800,backgroundColor=F2F2F2,modelId=2300,crop=detail.jpg');
+'https://image.spreadshirtmedia.com/image-server/v1/mp/productTypes/943/views/2/appearances/231,width=800,height=800,backgroundColor=F2F2F2,modelId=2300,crop=detail.jpg'),
 
-INSERT INTO PRODUCTS (title, category, price, image1,image2,image3,image4) VALUES('i came','men',20,
+('i came','men',20,
 'https://image.spreadshirtmedia.com/image-server/v1/mp/products/T210A2MPA3176PT17X0Y21D1009964639FS5585/views/1,width=800,height=800,appearanceId=2,backgroundColor=F2F2F2,modelId=1543,crop=detail/funny-i-came-i-saw-i-made-it-awkward-mens-t-shirt.jpg',
 'https://image.spreadshirtmedia.com/image-server/v1/mp/products/T210A2MPA3176PT17X0Y21D1009964639FS5585/views/1,width=800,height=800,appearanceId=2,backgroundColor=F2F2F2,modelId=1147,crop=detail/funny-i-came-i-saw-i-made-it-awkward-mens-t-shirt.jpg',
 'https://image.spreadshirtmedia.com/image-server/v1/mp/productTypes/210/views/2/appearances/2,width=800,height=800,backgroundColor=F2F2F2,modelId=1549,crop=detail.jpg',
-'https://image.spreadshirtmedia.com/image-server/v1/mp/productTypes/210/views/2/appearances/2,width=800,height=800,backgroundColor=F2F2F2,modelId=1153,crop=detail.jpg');
+'https://image.spreadshirtmedia.com/image-server/v1/mp/productTypes/210/views/2/appearances/2,width=800,height=800,backgroundColor=F2F2F2,modelId=1153,crop=detail.jpg'),
 
-INSERT INTO PRODUCTS (title, category, price, image1,image2,image3) VALUES('why so salty','men',24,'https://image.spreadshirtmedia.com/image-server/v1/mp/products/T812A2MPA3140PT17X11Y73D1023033403FS3861/views/1,width=650,height=800,appearanceId=2,backgroundColor=F2F2F2,modelId=115,crop=detail/why-so-salty-funny-salt-shaker-salty-attitude-mens-premium-t-shirt.jpg',
+('why so salty','men',24,'https://image.spreadshirtmedia.com/image-server/v1/mp/products/T812A2MPA3140PT17X11Y73D1023033403FS3861/views/1,width=650,height=800,appearanceId=2,backgroundColor=F2F2F2,modelId=115,crop=detail/why-so-salty-funny-salt-shaker-salty-attitude-mens-premium-t-shirt.jpg',
 'https://image.spreadshirtmedia.com/image-server/v1/mp/products/T812A2MPA3140PT17X11Y73D1023033403FS3861/views/1,width=650,height=800,appearanceId=2,backgroundColor=F2F2F2,modelId=1237,crop=detail/why-so-salty-funny-salt-shaker-salty-attitude-mens-premium-t-shirt.jpg',
 'https://image.spreadshirtmedia.com/image-server/v1/mp/productTypes/812/views/2/appearances/2,width=650,height=800,backgroundColor=F2F2F2,modelId=127,crop=detail.jpg',
-'https://image.spreadshirtmedia.com/image-server/v1/mp/productTypes/812/views/2/appearances/2,width=650,height=800,backgroundColor=F2F2F2,modelId=1243,crop=detail.jpg');
+'https://image.spreadshirtmedia.com/image-server/v1/mp/productTypes/812/views/2/appearances/2,width=650,height=800,backgroundColor=F2F2F2,modelId=1243,crop=detail.jpg'),
 
-INSERT INTO PRODUCTS (title, category, price, image1,image2,image3) VALUES('Tesla','men',50,
-'https://image.spreadshirtmedia.com/image-server/v1/mp/products/T20A2MPA3631PT17X41Y47D12443631FS9486CxD31818/views/1,width=800,height=800,appearanceId=2,backgroundColor=F2F2F2,modelId=2516,crop=detail/tesla-mens-premium-hoodie.jpg',
-'https://image.spreadshirtmedia.com/image-server/v1/mp/products/T951A54MPA3076PT17X0Y54D12443631FS9486CxD31818/views/1,width=800,height=800,appearanceId=54,backgroundColor=F2F2F2/tesla-unisex-baseball-t-shirt.jpg',
-'https://image.spreadshirtmedia.com/image-server/v1/mp/productTypes/20/views/2/appearances/2,width=800,height=800,backgroundColor=F2F2F2,modelId=2522,crop=detail.jpg');
 
-INSERT INTO PRODUCTS (title, category, price, image1,image2,image3,image4) VALUES('looney','women',19,
+('looney','women',19,
 'https://image.spreadshirtmedia.com/image-server/v1/mp/products/T347A231MPA2978PT17X1Y22D1034012890FS2007/views/1,width=800,height=800,appearanceId=231,backgroundColor=F2F2F2,modelId=2564,crop=detail/looney-tunes-tasmanian-devil-womens-t-shirt.jpg',
 'https://image.spreadshirtmedia.com/image-server/v1/mp/products/T347A231MPA2978PT17X1Y22D1034012890FS2007/views/1,width=800,height=800,appearanceId=231,backgroundColor=F2F2F2,modelId=2528,crop=detail/looney-tunes-tasmanian-devil-womens-t-shirt.jpg',
 'https://image.spreadshirtmedia.com/image-server/v1/mp/productTypes/347/views/2/appearances/231,width=800,height=800,backgroundColor=F2F2F2,modelId=2570,crop=detail.jpg',
-'https://image.spreadshirtmedia.com/image-server/v1/mp/productTypes/347/views/2/appearances/231,width=800,height=800,backgroundColor=F2F2F2,modelId=2534,crop=detail.jpg');
+'https://image.spreadshirtmedia.com/image-server/v1/mp/productTypes/347/views/2/appearances/231,width=800,height=800,backgroundColor=F2F2F2,modelId=2534,crop=detail.jpg'),
 
-INSERT INTO PRODUCTS (title, category, price, image1,image2,image3,image4) VALUES('face of roses','women',24,
+('face of roses','women',24,
 'https://image.spreadshirtmedia.com/image-server/v1/mp/products/T1196A1MPA3058PT17X36Y19D1016088063FS9256/views/1,width=650,height=800,appearanceId=1,backgroundColor=F2F2F2,modelId=5421,crop=detail/face-of-roses-womens-5050-t-shirt.jpg',
 'https://image.spreadshirtmedia.com/image-server/v1/mp/products/T1196A1MPA3058PT17X36Y19D1016088063FS9256/views/1,width=650,height=800,appearanceId=1,backgroundColor=F2F2F2,modelId=5386,crop=detail/face-of-roses-womens-5050-t-shirt.jpg',
 'https://image.spreadshirtmedia.com/image-server/v1/mp/productTypes/1196/views/2/appearances/1,width=650,height=800,backgroundColor=F2F2F2,modelId=5423,crop=detail.jpg',
-'https://image.spreadshirtmedia.com/image-server/v1/mp/productTypes/1196/views/2/appearances/1,width=650,height=800,backgroundColor=F2F2F2,modelId=5388,crop=detail.jpg');
+'https://image.spreadshirtmedia.com/image-server/v1/mp/productTypes/1196/views/2/appearances/1,width=650,height=800,backgroundColor=F2F2F2,modelId=5388,crop=detail.jpg'),
 
-INSERT INTO PRODUCTS (title, category, price, image1,image2,image3,image4) VALUES('Bright Side','women',24,
+('Bright Side','women',24,
 'https://image.spreadshirtmedia.com/image-server/v1/mp/products/T813A386MPA3132PT17X37Y8D1025343924FS1752/views/1,width=650,height=800,appearanceId=386,backgroundColor=F2F2F2,modelId=338,crop=detail/bright-side-womens-premium-t-shirt.jpg',
 'https://image.spreadshirtmedia.com/image-server/v1/mp/products/T813A386MPA3132PT17X37Y8D1025343924FS1752/views/1,width=650,height=800,appearanceId=386,backgroundColor=F2F2F2,modelId=2468,crop=detail/bright-side-womens-premium-t-shirt.jpg',
 'https://image.spreadshirtmedia.com/image-server/v1/mp/productTypes/813/views/2/appearances/386,width=650,height=800,backgroundColor=F2F2F2,modelId=350,crop=detail.jpg',
 'https://image.spreadshirtmedia.com/image-server/v1/mp/productTypes/813/views/2/appearances/386,width=650,height=800,backgroundColor=F2F2F2,modelId=2474,crop=detail.jpg');
-        
-INSERT INTO PRODUCTS (title, category, price, image1,image2,image3) VALUES('Cool Mom Funny Mothers Gift','women',35,
+
+INSERT INTO PRODUCTS (title, category, price, image1, image2,image3) 
+('Cool Mom Funny Mothers Gift','women',35,
 'https://image.spreadshirtmedia.com/image-server/v1/mp/products/T951A550MPA3076PT17X0Y24D1021763085FS2951/views/1,width=650,height=800,appearanceId=550,backgroundColor=F2F2F2/cool-mom-funny-mothers-gift-unisex-baseball-t-shirt.jpg',
 'https://image.spreadshirtmedia.com/image-server/v1/mp/products/T951A550MPA3076PT17X0Y24D1021763085FS2951/views/1,width=650,height=800,appearanceId=550,backgroundColor=F2F2F2,modelId=4892,crop=detail/cool-mom-funny-mothers-gift-unisex-baseball-t-shirt.jpg',
-'https://image.spreadshirtmedia.com/image-server/v1/mp/productTypes/951/views/2/appearances/550,width=650,height=800,backgroundColor=F2F2F2,modelId=4898,crop=detail.jpg');
+'https://image.spreadshirtmedia.com/image-server/v1/mp/productTypes/951/views/2/appearances/550,width=650,height=800,backgroundColor=F2F2F2,modelId=4898,crop=detail.jpg'),
 
-INSERT INTO PRODUCTS (title, category, price, image1) VALUES('pink ladybug','kids',13,
-'https://image.spreadshirtmedia.com/image-server/v1/mp/products/T48A22MPA2954PT17X14Y1D1002013047FS4390/views/1,width=650,height=800,appearanceId=22,backgroundColor=F2F2F2/pink-ladybug-big-sister-kids-t-shirt.jpg');
+('Tesla','men',50,
+'https://image.spreadshirtmedia.com/image-server/v1/mp/products/T20A2MPA3631PT17X41Y47D12443631FS9486CxD31818/views/1,width=800,height=800,appearanceId=2,backgroundColor=F2F2F2,modelId=2516,crop=detail/tesla-mens-premium-hoodie.jpg',
+'https://image.spreadshirtmedia.com/image-server/v1/mp/products/T951A54MPA3076PT17X0Y54D12443631FS9486CxD31818/views/1,width=800,height=800,appearanceId=54,backgroundColor=F2F2F2/tesla-unisex-baseball-t-shirt.jpg',
+'https://image.spreadshirtmedia.com/image-server/v1/mp/productTypes/20/views/2/appearances/2,width=800,height=800,backgroundColor=F2F2F2,modelId=2522,crop=detail.jpg');
 
-INSERT INTO PRODUCTS (title, category, price, image1) VALUES('Hockeysaurus Hockey','kids',15,
+
+INSERT INTO PRODUCTS (title, category, price, image1) 
+VALUES('pink ladybug','kids',13,
+'https://image.spreadshirtmedia.com/image-server/v1/mp/products/T48A22MPA2954PT17X14Y1D1002013047FS4390/views/1,width=650,height=800,appearanceId=22,backgroundColor=F2F2F2/pink-ladybug-big-sister-kids-t-shirt.jpg'),
+
+('Hockeysaurus Hockey','kids',15,
 'https://image.spreadshirtmedia.com/image-server/v1/mp/products/T48A2MPA2954PT17X0Y1D1018577076FS2620/views/1,width=650,height=800,appearanceId=2,backgroundColor=F2F2F2/hockeysaurus-hockey-cute-funny-kids-dinosaur-gift-kids-t-shirt.jpg');
 
 
-INSERT INTO CART (username, id, quantity) VALUES ('noorin',5,2);
-INSERT INTO CART (username, id, quantity) VALUES ('elham',2,1);
+INSERT INTO CART (username, id, quantity) VALUES
+ ('noorin',5,2),
+ ('elham',2,1);

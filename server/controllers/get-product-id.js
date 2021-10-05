e59ac2b6d2408a49eb74e57524abe18e91c784d0
divluffy <div.jo2022@gmail.com>
2021-10-05 15:51:25 +0300

    router product

--- server/controllers/get-product-id.js
+++ server/controllers/get-product-id.js
@@ -3,7 +3,7 @@ const getProductQuery = require('../database/queries/get-product');
 const getProduct = (req, res, next) => {
   const { productId } = req.params;
   getProductQuery(productId)
-    .then((data) => {
+    .then((rows) => {
       res.json({ data: rows });
     })
     .catch((err) => next(err));

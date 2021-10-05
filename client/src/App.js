421349b9b00f9cd2327377b6d9d9c0da2ebbe5e8
AhemdAboAbadi <ahmed.free.py@gmail.com>
2021-10-05 19:10:43 +0300

    Add Header & Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import "antd/dist/antd.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App">Hello Team Crown-Ecommerce !!</div>;
+  return (
+    <div className="App">
+      <Header />
+      Hi from App
+      <Footer />
+    </div>
+  );
 }
 
 export default App;

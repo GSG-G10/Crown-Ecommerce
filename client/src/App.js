44a94ae8a5619a131956640fd99f440544f0e138
ElhamFadel <elham2000Fadel@gmail.com>
2021-10-06 07:32:38 +0300

    remove test

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <form method="post" action="/api/v1/login">
-        <input type="email" placeholder="Enter your email" name="email" />
-        <input
-          type="password"
-          placeholder="Enter your password"
-          name="password"
-        />
-        <button>Submit</button>
-      </form>
-      <form method="post" action="/api/v1/cart/add-to-cart/2">
-        <input type="email" placeholder="Enter your email" name="email" />
-
-        <button>Submit</button>
-      </form>
-      <form method="post" action="/api/v1/cart/delete-product/2">
-        <input type="email" placeholder="Enter your email" name="email" />
-
-        <button>Submit</button>
-      </form>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;

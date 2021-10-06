4f41c4f15c59f16153817833ce063955e160ec2c
MohammadHMansour <mdmansour@gmail.com>
2021-10-06 10:50:20 +0300

    temporal commit

--- server/controllers/signupControllers/signup.js
+++ server/controllers/signupControllers/signup.js
@@ -14,10 +14,9 @@ const signup = (req, res) => {
     // eslint-disable-next-line no-throw-literal
     if (exists) { throw { message: exists, cause: 'validation error' }; }
   }).then(() => createUser(userName, email, firstName, password))
-    .then(() => {
-      createCookies(res, userName, true, createSession(userName));
-      res.redirect('/');
-    })
+    .then(() => createSession(userName))
+    .then((token) => createCookies(res, userName, true, token))
+    .then(() => res.status(201).json({ message: 'user registered successfully' }))
     .catch((err) => {
       if (!err.cause) { res.status(500).json({ message: 'internal server error' }); } else res.status(403).json({ message: err.message });
     });

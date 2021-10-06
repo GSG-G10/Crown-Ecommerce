0e619f4edfda3873cf43e187d9422fe78e059b8b
divluffy <div.jo2022@gmail.com>
2021-10-06 15:17:17 +0300

    fix

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,9 +1,7 @@
 const express = require('express');
 
 const product = express.Router();
-const { getProductId } = require('../controllers');
-const { fetchProducts } = require('../controllers');
-const { getRandomProduct } = require('../controllers');
+const { getProductId, fetchProducts, getRandomProduct } = require('../controllers');
 
 product.get('/more', getRandomProduct);
 product.get('/:productId', getProductId);

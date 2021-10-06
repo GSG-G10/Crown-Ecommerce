7dadf9c94fa09e0148b7f7743e1b726291096996
Your Name <noorin.sa.99@gmail.com>
2021-10-06 11:22:46 +0300

    fix issue Relates #55

--- server/database/queries/getDataPostQuery.js
+++ server/database/queries/getDataPostQuery.js
@@ -1,5 +1,5 @@
 const connection = require('../connection');
 
-const filterSearch = (searchWord) => connection.query(`SELECT * FROM PRODUCTS WHERE title LIKE'%${searchWord}%'`);
+const filterSearch = (searchWord) => connection.query('SELECT * FROM PRODUCTS WHERE title LIKE $1', [`%${searchWord}%`]);
 
 module.exports = filterSearch;

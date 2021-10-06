4799d243d9c2b10996708c96187fc677d4796f22
divluffy <div.jo2022@gmail.com>
2021-10-06 15:10:41 +0300

    fix

--- server/database/queries/get-product-random.js
+++ server/database/queries/get-product-random.js
@@ -1,4 +1,4 @@
 const connection = require('../connection');
 
-const handleRandom = async () => connection.query('SELECT id, image1 FROM PRODUCTS ORDER BY RANDOM() LIMIT 4');
+const handleRandom = () => connection.query('SELECT id, image1 FROM PRODUCTS ORDER BY RANDOM() LIMIT 4');
 module.exports = handleRandom;

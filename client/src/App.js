d373ccfb513ec4c51ff7b6e60348e8245fefe36c
ElhamFadel <elham2000Fadel@gmail.com>
2021-10-07 13:03:59 +0300

    add card

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import { Home } from "./pages";
 import "./App.css";
 import "antd/dist/antd.css";
-
+import { BrowserRouter as Router } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Home />
+      </div>
+    </Router>
   );
 }
 
--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,21 +1,33 @@
 import { Card } from "antd";
+import { AiOutlineHeart } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import "./style.css";
 const { Meta } = Card;
-
 function CardWrapper({
   key,
   data: { id, title, category, image1, price, onClick },
 }) {
-  console.log(image1);
   return (
-    <div className="container-card" id={id}>
-      <Card
-        hoverable
-        style={{ width: 240 }}
-        cover={<img alt={title} src={image1} />}
-      >
-        <Meta title={title} description="Crown-Ecommerce.com" />
-      </Card>
-    </div>
+    <Link to={`/product/${id}`}>
+      <div className="container-card" id={id}>
+        <Card
+          hoverable
+          style={{ width: 240 }}
+          cover={<img alt={title} src={image1} />}
+        >
+          <div className="container-card-title">
+            <Meta title={title} />
+            <h4>{price}</h4>
+            <h5>for {category}</h5>
+          </div>
+          <div className="container-card-icon">
+            <button onClick={onClick}>
+              <AiOutlineHeart />
+            </button>
+          </div>
+        </Card>
+      </div>
+    </Link>
   );
 }
 

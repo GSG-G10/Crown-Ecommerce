830e6c6d394af6764b00f458c06349e12eed70b6
MohammadHMansour <mdmansour@gmail.com>
2021-10-07 14:14:00 +0300

    created login page

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,13 @@ import { Home } from "./pages";
 import "./App.css";
 import Footer from "./components/Footer";
 import Product from "./pages/Product";
-import { Complete } from "./components";
+import { Complete, Login } from "./components";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <Router>
-       <Home />
+        <Route exact path="/login" component={Login} />
         <Route exact path="/" component={Footer} />
         <Route exact path="/search" component={Complete} />
         <Route exact path="/product/:id" component={Product} />

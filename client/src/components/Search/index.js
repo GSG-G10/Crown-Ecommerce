525ff998a3baf1f4416cb3f79b8e69d070a51d48
Your Name <noorin.sa.99@gmail.com>
2021-10-07 10:21:56 +0300

    search component Relates #61

---
+++ client/src/components/Search/index.js
@@ -0,0 +1,57 @@
+import { Input, AutoComplete } from "antd";
+import React, { useState, useEffect } from "react";
+import "./Search.css";
+import { useHistory } from "react-router-dom";
+
+const Complete = () => {
+  let history = useHistory();
+  console.log(history);
+  const [query, setQuery] = useState(" ");
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    if (query !== " ") {
+      fetch(`/api/v1/search/${query}`, { mode: "cors" })
+        .then((result) => {
+          return result.json();
+        })
+        .then((data) =>
+          data.map(({ id, title }) => {
+            return { value: id, label: title };
+          })
+        )
+        .then((data) => setProduct(data))
+        .catch((err) => {
+          return <p>{err}</p>;
+        });
+    }
+  }, [query]);
+
+  const onSearch = (value) => {
+    setQuery(value);
+  };
+
+  const onSelect = (value) => {
+    history.push(`/product/${value}`);
+  };
+
+  return (
+    <>
+      <AutoComplete
+        dropdownClassName="certain-category-search-dropdown"
+        dropdownMatchSelectWidth={250}
+        style={{ width: 250 }}
+        options={product}
+        onSelect={onSelect}
+      >
+        <Input.Search
+          placeholder="Search for a product"
+          onSearch={onSearch}
+          allowClear
+        />
+      </AutoComplete>
+    </>
+  );
+};
+
+export default Complete;
